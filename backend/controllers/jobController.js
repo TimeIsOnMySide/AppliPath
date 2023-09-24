1d69b67c796653dce559c9738944dc18e3f8e3e6
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-09-24 11:04:14 -0700

    got all initial routes tested & working. No authentication yet.

---
+++ backend/controllers/jobController.js
@@ -0,0 +1,68 @@
+const Job = require('../models/jobModel');
+const mongoose = require('mongoose');
+
+// Get all Jobs
+const getJobs = async (req, res) => {
+    const jobs = await Job.find({});
+    res.status(200).json(jobs);
+};
+
+// POST a new job
+const createJob = async (req, res) => {
+    const { jobTitle, companyName, jobPostingURL, salary, location, notes } =
+        req.body;
+    try {
+        const job = await Job.create({
+            jobTitle,
+            companyName,
+            jobPostingURL,
+            salary,
+            location,
+            notes,
+        });
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// DELETE an existing job
+const deleteJob = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Job not found' });
+    }
+    const job = await Job.findOneAndDelete({ _id: id });
+
+    if (!job) {
+        return res.status(400).json({ error: 'Job not found' });
+    }
+
+    res.status(200).json(job);
+};
+
+// UPDATE an existing workout
+const updateJob = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Job not found' });
+    }
+    const job = await Job.findOneAndUpdate(
+        { _id: id },
+        {
+            ...req.body,
+        }
+    );
+    if (!job) {
+        return res.status(400).json({ error: 'Job not found' });
+    }
+    res.status(200).json(job);
+};
+
+// Export all the functions to be used in the routes.js file
+module.exports = {
+    createJob,
+    getJobs,
+    deleteJob,
+    updateJob,
+};
--- backend/routes.js
+++ backend/routes.js
@@ -1,42 +1,25 @@
 // These are all the routes for my node server. I put them in a seperate files to keep the server.js file clean
 
 const express = require('express');
-const Job = require('./models/jobModel');
+const {
+    createJob,
+    getJobs,
+    deleteJob,
+    updateJob,
+} = require('./controllers/jobController');
 
 const router = express.Router();
 
 // GET all job cards
-router.get('/jobs', (req, res) => {
-    res.json({ mssg: 'GET all job cards' });
-});
+router.get('/jobs', getJobs);
 
 // POST a new job card
-router.post('/jobs', async (req, res) => {
-    const { jobTitle, companyName, jobPostingURL, salary, location, notes } =
-        req.body;
-    try {
-        const job = await Job.create({
-            jobTitle,
-            companyName,
-            jobPostingURL,
-            salary,
-            location,
-            notes,
-        });
-        res.status(200).json(job);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post('/jobs', createJob);
 
 // DELETE a job card
-router.delete('/jobs/:id', (req, res) => {
-    res.json({ mssg: 'DELETE a job card' });
-});
+router.delete('/jobs/:id', deleteJob);
 
 // UPDATE a job card
-router.patch('/jobs/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE a job card' });
-});
+router.patch('/jobs/:id', updateJob);
 
 module.exports = router;
