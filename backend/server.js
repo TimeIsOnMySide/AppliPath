6bfb572765d1d5ed944088f3684222c645b17bd4
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-10-21 10:23:11 -0700

    Added maps to server side. Need to add to frontend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require('express'); // require express package that I installed 
 const mongoose = require('mongoose');
 require('dotenv').config(); // Environment variables
 const cors = require('cors');
+const fetch = require('node-fetch'); // Import node-fetch
 const app = express(); // Creates an express app and store it in 'app'
 const routes = require('./routes');
 
@@ -32,3 +33,20 @@ mongoose
     .catch((error) => {
         console.log(error);
     });
+
+// Create a route to handle address geocoding
+app.get('/geocode', async (req, res) => {
+    const address = req.query.address;
+
+    try {
+        const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+        );
+        const data = await response.json();
+        const location = data.results[0].geometry.location;
+        res.json(location);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to geocode address' });
+    }
+});
